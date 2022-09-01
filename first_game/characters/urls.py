2b68d1b382e873cd523429d8c9ffb73ab0925cec
Spydirwebb <webb.jeffery.89@gmail.com>
2022-09-01 20:33:00 +0000

    part 3 done for game

--- first_game/characters/urls.py
+++ first_game/characters/urls.py
@@ -2,6 +2,10 @@ from django.urls import path
 
 from . import views
 
+app_name= 'characters'
 urlpatterns = [
-    path('', views.index, name='index')
+    #ex: /character/
+    path('', views.index, name='index'),
+    #ex: /character/1
+    path('<int:character_id>/', views.details, name='details'),
 ]
--- first_game/characters/views.py
+++ first_game/characters/views.py
@@ -1,6 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+
+from .models import Character
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello from Character!")
+    character_list = Character.objects.order_by('-name')
+    context = {'character_list': character_list}
+    return render(request, 'characters/index.html', context)
+
+def details (request, character_id):
+    character = get_object_or_404(Character, pk=character_id)
+    return render(request, 'characters/detail.html', {'character': character})
