cebe1ff4a099d42582c89e992af86340c8ad80c8
Spydirwebb <webb.jeffery.89@gmail.com>
2022-09-27 21:39:06 +0000

    added comment

--- django-react-app/frontend/src/App.js
+++ django-react-app/frontend/src/App.js
@@ -46,6 +46,7 @@ const App = () => {
     await axios
       //.get('http://localhost:8000/api/todos/')
       .get('https://8000-spydirwebb-django-canuz7re0z4.ws-us67.gitpod.io/api/todos/')
+      /////does the URL change?
       //.get('http://8000-spydirwebb-django-canuz7re0z4.ws-us65.gitpod.io/api/todos/')
       //.get('http://127.0.0.1:8000/api/todos/')
       .then(res => {
