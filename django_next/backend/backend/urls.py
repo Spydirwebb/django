3ceabeb66672c7c39bf49545c07801adf93b20a7
Spydirwebb <webb.jeffery.89@gmail.com>
2023-01-04 07:31:58 +0000

    finished visit 2

--- django_next/backend/backend/urls.py
+++ django_next/backend/backend/urls.py
@@ -6,6 +6,9 @@ from reviews import views
 router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet)
 router.register(r'groups', views.GroupViewSet)
+router.register(r'reviews', views.ReviewsViewSet)
+router.register(r'businesses', views.BusinessViewSet)
+router.register(r'categories', views.CategoryViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
--- django_next/backend/reviews/views.py
+++ django_next/backend/reviews/views.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.models import User, Group
 from rest_framework import viewsets, permissions
-from reviews.serializers import UserSerializer, GroupSerializer
+from reviews.serializers import UserSerializer, GroupSerializer, ReviewSerializer, BusinessSerializer, CategorySerializer
+from reviews.models import Review, Business, Category
 
 # Create your views here.
 class UserViewSet(viewsets.ModelViewSet):
@@ -8,8 +9,22 @@ class UserViewSet(viewsets.ModelViewSet):
     serializer_class = UserSerializer
     permission_classes = [permissions.IsAuthenticated]
 
-# Create your views here.
 class GroupViewSet(viewsets.ModelViewSet):
     queryset = Group.objects.all()
     serializer_class = GroupSerializer
     permission_classes = [permissions.IsAuthenticated]
+
+class ReviewsViewSet(viewsets.ModelViewSet):
+    queryset = Review.objects.all()
+    serializer_class = ReviewSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+class BusinessViewSet(viewsets.ModelViewSet):
+    queryset = Business.objects.all()
+    serializer_class = BusinessSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
