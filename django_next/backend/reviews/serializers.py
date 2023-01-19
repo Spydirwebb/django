e0921a348dd62cff96f0d2e680ea729762643690
Spydirwebb <webb.jeffery.89@gmail.com>
2023-01-19 08:22:35 +0000

    video 5 done

--- django_next/backend/reviews/serializers.py
+++ django_next/backend/reviews/serializers.py
@@ -3,50 +3,50 @@ from rest_framework import serializers
 from reviews.models import Review, Business, Category
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = User
-        fields = ['url', 'username', 'email', 'groups']
+	class Meta:
+		model = User
+		fields = ['url', 'username', 'email', 'groups']
 
 class GroupSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = Group
-        fields = ['url', 'name']
+	class Meta:
+		model = Group
+		fields = ['url', 'name']
 
 class ReviewSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = Review
-        fields = "__all__"
+	class Meta:
+		model = Review
+		fields = '__all__'
 
 class BusinessSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = Business
-        depth = 1
-        fields = [
-            'url',
-            'name',
-            'slug',
-            'description',
-            'price_range',
-            'street_address',
-            'city',
-            'region',
-            'postal_code',
-            'country',
-            'website',
-            'phone',
-            'hours',
-            'reviews'
-        ]
+	class Meta:
+		model = Business
+		depth = 1
+		fields = [
+			'url',
+			'name',
+			'slug',
+			'description',
+			'price_range',
+			'street_address',
+			'city',
+			'region',
+			'postal_code',
+			'country',
+			'website',
+			'phone',
+			'hours',
+			'reviews',
+		]
 
 class CategorySerializer(serializers.HyperlinkedModelSerializer):
-    business = BusinessSerializer(many=True)
-    class Meta:
-        model = Category
-        depth = 1
-        fields = [
-            'url',
-            'name',
-            'slug',
-            'ordinal',
-            'business'
-        ]
+	business = BusinessSerializer(many=True)
+	class Meta:
+		model = Category
+		depth = 1
+		fields = [
+			'url',
+			'name',
+			'slug',
+			'ordinal',
+			'business'
+		]
--- django_next/frontend/pages/categories/[slug].js
+++ django_next/frontend/pages/categories/[slug].js
@@ -6,7 +6,7 @@ import {styled} from '@mui/system'
 import axios from 'axios'
 import { useRouter } from 'next/router'
 
-const Category = ({ category, avgReview }) => {
+const Category = ({ category, reviewAvgs }) => {
     const router = useRouter()
     
     const handleBusinessClick = business => {
@@ -21,7 +21,7 @@ const Category = ({ category, avgReview }) => {
                 </Grid>
                 <Grid item xs={12} md={9}>
                     {category.business.map(business => (
-                    <Card className="card" onClick={() => handleBusinessClick(business)}>
+                    <Card className="card" onClick={() => handleBusinessClick(business)} key={business.id}>
                         <Box>
                             <CardContent>
                                 <Grid container>
@@ -33,7 +33,7 @@ const Category = ({ category, avgReview }) => {
                                         <Typography variant='subtitle1' className="subtitle">{business.description}</Typography>
                                     </Grid>
                                     <Grid item xs={6}>
-                                        <Typography variant='h5'><AverageReview value={avgReview}/></Typography>
+                                        <Typography variant='h5'><AverageReview value={reviewAvgs[business.url]}/></Typography>
                                         <Typography variant="subtitle1">{business.hours}</Typography>
                                         <Typography variant="subtitle1">{business.street_address} {business.city}, {business.region} {business.postal_code} {business.country}</Typography>
                                     </Grid>
@@ -50,35 +50,20 @@ const Category = ({ category, avgReview }) => {
 }
 export async function getServerSideProps({ query: {slug} }) {
     let { data } = await axios.get(`${process.env.DB_BASE_URL}/categories/?slug=${slug}`)
-
-    let avgReview = null
-    let business = data[0].business[0]
-    
-    //get count of reviews (length doesn't work for some reason. business.reviews is an object)
-    let reviewCount=0
+    //console.log(data[0])
+    let reviewAvgs = {}
     let i=0
-    while (business.reviews[i]){
-        reviewCount+=1
+    while(data[0].business[i]){
+        let business = data[0].business[i]
+        let average = getReviewAverage(business)
+        console.log('stars: ' + average)
+        reviewAvgs[business.url] = average
         i+=1
     }
-
-    //calculate average stars
-    if (data && business.reviews) {
-        let totalReviewsStars = 0
-        for (let i=0; i < reviewCount; i++){
-            totalReviewsStars = totalReviewsStars + Number(business.reviews[i].stars)
-        }
-
-        const inverse = 1/2
-
-        avgReview = Math.round((totalReviewsStars / reviewCount) / inverse) * inverse
-    }
-    console.log('stars: ' + avgReview)
-
     return {
       props: {
         category: data[0] || null,
-        avgReview: avgReview
+        reviewAvgs: reviewAvgs
       }
     }
   }
@@ -95,3 +80,22 @@ const CategoryStyled = styled('Category')({
     }
 })
 export default Category
+
+const getReviewAverage = (business) => {
+    let reviewAvg = null
+    //get count of reviews (length doesn't work for some reason. business.reviews is an object)
+    let reviewCount=0
+    let totalReviewsStars = 0
+    let i=0
+    while (business.reviews[i]){
+        reviewCount+=1
+        totalReviewsStars = totalReviewsStars + Number(business.reviews[i].stars)
+        
+        i+=1
+    }
+    const inverse = 1/2
+
+    reviewAvg = Math.round((totalReviewsStars / reviewCount) / inverse) * inverse
+
+    return reviewAvg
+}
